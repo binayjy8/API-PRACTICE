75b3a69de44b586e2fcf2c45ed4b648aae631f3c
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-16 09:34:27 +0530

    code

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 let url = "https://official-joke-api.appspot.com/jokes/programming/random";
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async ()=> {
+    let res = await axios.get(url);
+})
